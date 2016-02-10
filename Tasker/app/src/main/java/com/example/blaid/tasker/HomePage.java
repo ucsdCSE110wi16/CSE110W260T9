42f3299c95f8b37a9a0308619c13f3a23a88ec64
kcriddle <kcriddle@ucsd.edu>
2016-02-10 00:48:33 -0800

    Added Settings Activity
    User can now navigate from the home page to the settings page in the UI
    shell

--- Tasker/app/src/main/java/com/example/blaid/tasker/HomePage.java
+++ Tasker/app/src/main/java/com/example/blaid/tasker/HomePage.java
@@ -1,15 +1,19 @@
 package com.example.blaid.tasker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-import com.example.blaid.tasker.R;
+
+
 
 public class HomePage extends AppCompatActivity {
+
+    Button SettingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,8 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+
+        SettingsButton = (Button) findViewById(R.id.settingsButton);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +32,16 @@ public class HomePage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        SettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settingsPage(v);
+            }
+        });
     }
 
+    public void settingsPage(View view) {
+        startActivity(new Intent(HomePage.this, settings.class));
+    }
 }
--- Tasker/app/src/main/java/com/example/blaid/tasker/Login.java
+++ Tasker/app/src/main/java/com/example/blaid/tasker/Login.java
@@ -71,7 +71,6 @@ public class Login extends AppCompatActivity {
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
 
-        title = (TextView) findViewById(R.id.apptitle);
         forgotUsername = (TextView) findViewById(R.id.textView2);
         forgotPassword = (TextView) findViewById(R.id.textView3);
 
@@ -89,6 +88,7 @@ public class Login extends AppCompatActivity {
                 signup(v);
             }
         });
+
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -159,16 +159,6 @@ public class Login extends AppCompatActivity {
     }
 
     public void signin(View view) {
-//        if (username.getText().toString().equals("admin")){
-//            if (password.getText().toString().equals("admin")){
-//                Intent intent = new Intent(Login.this, HomePage.class);
-//                Toast.makeText(getApplicationContext(), "Loging in...", Toast.LENGTH_SHORT).show();
-//
-//                startActivity(intent);
-//            }
-//        } else {
-//            Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-//        }
 
         // Retrieve the text entered from the EditText
         usernameTxt = username.getText().toString();
@@ -201,30 +191,8 @@ public class Login extends AppCompatActivity {
     }
 
     public void signup(View view) {
-        Intent intent = new Intent(Login.this, CreateAccount.class);
-        startActivity(intent);
-//        usernameTxt = username.getText().toString();
-//        passwordTxt = password.getText().toString();
-//
-//        if (usernameTxt.equals("") && passwordTxt.equals("")) {
-//            Toast.makeText(getApplicationContext(), "FILL IN THE INFO!", Toast.LENGTH_LONG).show();
-//        } else {
-//            ParseUser user = new ParseUser();
-//            user.setUsername(usernameTxt);
-//            user.setPassword(passwordTxt);
-//            user.signUpInBackground(new SignUpCallback() {
-//                @Override
-//                public void done(ParseException e) {
-//                    if (e == null) {
-//                        Toast.makeText(getApplicationContext(), "Successfully Signed Up!", Toast.LENGTH_LONG).show();
-//                    } else {
-//                        Toast.makeText(getApplicationContext(), "Sign Up Error", Toast.LENGTH_LONG).show();
-//                    }
-//                }
-//            });
-//        }
-
-
+        startActivity(new Intent(Login.this, CreateAccount.class));
     }
+
 
 }
