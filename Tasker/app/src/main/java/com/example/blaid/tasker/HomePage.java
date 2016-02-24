88c4b6cd2ff5d5c97fec7fd3f17254d734153651
Chandler Blaid Burgess <blaidburgess@gmail.com>
2016-02-23 17:49:08 -0800

    Adding Create Task page

--- Tasker/app/src/main/java/com/example/blaid/tasker/HomePage.java
+++ Tasker/app/src/main/java/com/example/blaid/tasker/HomePage.java
@@ -5,11 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.blaid.tasker.R;
 
 public class HomePage extends AppCompatActivity {
 
@@ -42,6 +39,6 @@ public class HomePage extends AppCompatActivity {
     }
 
     public void goCreateTask(View v) {
-        startActivity(new Intent(getApplicationContext(), CreateTask.class));
+        startActivity(new Intent(getApplicationContext(), CreateT.class));
     }
 }
