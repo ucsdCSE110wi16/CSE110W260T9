0f15a6c336ff68399ce1ec11cc2533ec462741e4
j-mcc3093 <j.mcc3093@gmail.com>
2016-02-25 19:36:56 -0800

    redesigned new taks UI

--- Tasker/app/src/main/java/com/example/blaid/tasker/CreateTask.java
+++ Tasker/app/src/main/java/com/example/blaid/tasker/CreateTask.java
@@ -2,16 +2,13 @@ package com.example.blaid.tasker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+
 
 public class CreateTask extends AppCompatActivity {
 
-    Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +17,6 @@ public class CreateTask extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        done = (Button) findViewById(R.id.doneButton);
-
-        done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goHome(v);
-            }
-        });
     }
 
     public void goHome(View v) {
