ac016a9f1514dbaa6b50792639c26ac2b8a8b61b
j-mcc3093 <j.mcc3093@gmail.com>
2016-02-29 20:07:32 -0800

    added DatePickerDialog to CreateTask activity

--- Tasker/app/src/main/java/com/example/blaid/tasker/CreateTask.java
+++ Tasker/app/src/main/java/com/example/blaid/tasker/CreateTask.java
@@ -1,14 +1,38 @@
 package com.example.blaid.tasker;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
 
 
 public class CreateTask extends AppCompatActivity {
+    private int year = 2016;
+    private int month = 2;
+    private int day;
 
+    private DatePickerDialog.OnDateSetListener myDateListener =
+                                               new DatePickerDialog.OnDateSetListener() {
+         @Override
+         public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {}
+                                               };
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
