d0803640502e8471a34ae911bd18700caed4d116
kcriddle <kcriddle@ucsd.edu>
2016-03-08 00:59:54 -0800

    commit for jamie to look at error message march 8 at 1am

---
+++ Tasker/app/src/main/java/com/example/blaid/tasker/UserProfile.java
@@ -0,0 +1,65 @@
+package com.example.blaid.tasker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Login/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_logout:
+                Toast.makeText(UserProfile.this, "Logging out...", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserProfile.this, Login.class));
+                break;
+
+            case R.id.action_settings:
+                startActivity(new Intent(UserProfile.this, settings.class));
+                break;
+
+            case R.id.action_edit_profile:
+                startActivity(new Intent(UserProfile.this, User_Settings.class));
+                break;
+
+            case R.id.action_create_task:
+                startActivity(new Intent(UserProfile.this, CreateTask.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
