b60f9d079cdb12348ca345db25c26270641aaa34
kcriddle <kcriddle@ucsd.edu>
2016-03-08 20:11:56 -0800

    refactored names of XMLs

--- Tasker/app/src/main/java/com/example/blaid/tasker/EditProfile.java
+++ Tasker/app/src/main/java/com/example/blaid/tasker/EditProfile.java
@@ -9,16 +9,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class UserProfile extends AppCompatActivity {
+public class EditProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
+        setContentView(R.layout.activity_edit_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         setupActionBar();
+
     }
 
     private void setupActionBar() {
@@ -28,6 +29,7 @@ public class UserProfile extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -42,22 +44,30 @@ public class UserProfile extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.action_logout) {
+            return true;
+        }*/
         switch (id){
             case R.id.action_logout:
-                Toast.makeText(UserProfile.this, "Logging out...", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(UserProfile.this, Login.class));
+                Toast.makeText(EditProfile.this, "Logging out...", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(EditProfile.this, Login.class));
                 break;
 
             case R.id.action_settings:
-                startActivity(new Intent(UserProfile.this, SettingsNEW_Activity.class));
+                startActivity(new Intent(EditProfile.this, SettingsPage.class));
                 break;
 
             case R.id.action_edit_profile:
-                startActivity(new Intent(UserProfile.this, User_Settings.class));
+                Toast.makeText(EditProfile.this, "You are already viewing Edit Profile", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.action_create_task:
-                startActivity(new Intent(UserProfile.this, CreateTask.class));
+                startActivity(new Intent(EditProfile.this, CreateTask.class));
+                break;
+
+            case R.id.action_home_page:
+                startActivity(new Intent(EditProfile.this, Home_Page.class));
                 break;
         }
 
--- Tasker/app/src/main/java/com/example/blaid/tasker/ViewProfile.java
+++ Tasker/app/src/main/java/com/example/blaid/tasker/ViewProfile.java
@@ -2,26 +2,23 @@ package com.example.blaid.tasker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
-public class User_Settings extends AppCompatActivity {
+public class ViewProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user__settings);
+        setContentView(R.layout.activity_view_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         setupActionBar();
-
     }
 
     private void setupActionBar() {
@@ -31,7 +28,6 @@ public class User_Settings extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -46,30 +42,22 @@ public class User_Settings extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_logout) {
-            return true;
-        }*/
         switch (id){
             case R.id.action_logout:
-                Toast.makeText(User_Settings.this, "Logging out...", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(User_Settings.this, Login.class));
+                Toast.makeText(ViewProfile.this, "Logging out...", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ViewProfile.this, Login.class));
                 break;
 
             case R.id.action_settings:
-                startActivity(new Intent(User_Settings.this, SettingsNEW_Activity.class));
+                startActivity(new Intent(ViewProfile.this, SettingsPage.class));
                 break;
 
             case R.id.action_edit_profile:
-                Toast.makeText(User_Settings.this, "You are already viewing Edit Profile", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ViewProfile.this, EditProfile.class));
                 break;
 
             case R.id.action_create_task:
-                startActivity(new Intent(User_Settings.this, CreateTask.class));
-                break;
-
-            case R.id.action_home_page:
-                startActivity(new Intent(User_Settings.this, Home_Page.class));
+                startActivity(new Intent(ViewProfile.this, CreateTask.class));
                 break;
         }
 
